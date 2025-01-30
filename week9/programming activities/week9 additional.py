136161da0c5286932c3262346058efed6ff43368
pandawhite <andrewsmithzeng@gmail.com>
2025-01-29 17:44:03 -0700

    update20250129

--- week9/programming activities/week9 additional.py
+++ week9/programming activities/week9 additional.py
@@ -8,3 +8,20 @@ have been updated.
 - save the Python dictionary to person.json
 - open person.json and verify the "age" value has increased by 1
 """
+import json
+
+#Open the JSON file in read mode and load its content into a Python dictionary
+#json.load(file)	Reads JSON directly from a file object
+#json.loads(str)    Reads JSON from a string
+with open("week9\programming activities\person-1.json", "r") as file:
+    person = json.load(file)
+
+print(person)
+# Increment the value of "age" in dictionary by 1
+person["age"] += 1
+
+# Open the JSON file in write mode and save the updated dictionary back to the file
+with open("week9\programming activities\person-1.json", "w") as file:
+    json.dump(person, file)
+
+print(person)
