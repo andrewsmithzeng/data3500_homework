9c87806ea8c2c3b60d221455ba0602458ac3f756
Andy Zeng <andrewsmithzeng@gmail.com>
2025-04-05 03:03:10 -0600

    update20250405

--- week12/HW5/HW5 - Stock Market Trading Simulations.py
+++ week12/HW5/HW5 - Stock Market Trading Simulations.py
@@ -1,5 +1,7 @@
-import os,pandas, numpy, json
+import os, pandas, numpy, json
 
+
+#function 1: extract the closing prices from the csv files and save them to their corresponding txt files, all at once.
 def convert_clomn_to_text(folder_panth):
     for filename in os.listdir(folder_panth):
         if filename.endswith('.csv'):
@@ -13,13 +15,13 @@ def convert_clomn_to_text(folder_panth):
             with open(txt_path, 'w') as txt_file:
                 for value in values:
                     txt_file.write(f"{value}\n")
+#call the function 1 to convert the csv files to txt files
+# convert_clomn_to_text('week12\HW5')
 
-convert_clomn_to_text('week12\HW5')
 
-with open("week10/Nvda_1y_20250305.txt", 'r') as f:
-    prices = [float(line) for line in f]
-def MeanReversionStrategy(prices,name):
- first_buy = None
+#function 2: mean reversion strategy 
+def MeanReversionStrategy(prices,n):
+    first_buy = None
     buy = None
     sell = None
     trade_profit = 0
@@ -40,38 +42,91 @@ def MeanReversionStrategy(prices,name):
             buy = None # reset buy to None
 
         total_profit += trade_profit   
+     # in case there is no buy at all   
+    if first_buy is not None:
+        final_profit_percentage = ( total_profit / first_buy ) * 100
+    else:
+        first_buy = 0
+        final_profit_percentage = 0
 
-
-    final_profit_percentage = ( total_profit / first_buy ) * 100
     print(f'Total profit: {total_profit:.2f}')
-    print(f'fist buy: {first_buy}')
+    print(f'First buy: {first_buy}')
     print(f'% return: {final_profit_percentage:.2f}%')
+    
+    mr_profit = round(total_profit,2)
+    mr_return = round(final_profit_percentage,2)
+    return mr_profit, mr_return
 
+#function 3: simple moving average strategy
+def SimpleMovingAvg(prices, n):
+    first_buy = None
+    buy = None
+    sell = None
+    trade_profit = 0
+    total_profit = 0
 
+    for i in range(n,len(prices)-n+1):
+        # set condition to buy when holding no stock.
+        if buy == None and prices[i] > numpy.mean(prices[i-n:i]): # without numpy, use sum(prices[i-n:i])/n
+            buy = prices[i]
+            print(f'buying at {buy}')
+            if first_buy is None:
+                first_buy = buy
+        elif buy !=None and prices[i] < numpy.mean(prices[i-n:i]):
+            sell = prices[i]
+            trade_profit = sell - buy
+            print(f'selling at {sell}')
+            print(f'trade profit: {trade_profit:.2f}')
+            buy = None # reset buy to None
 
-def SimpleMovingAvg(prices, n,name):
-   
+        total_profit += trade_profit   
 
-moving_average(prices, 5)
+     # in case there is no buy at all   
+    if first_buy is not None:
+        final_profit_percentage = ( total_profit / first_buy ) * 100
+    else:
+        first_buy = 0
+        final_profit_percentage = 0
+
+    print(f'Total profit: {total_profit:.2f}')
+    print(f'First buy: {first_buy}')
+    print(f'% return: {final_profit_percentage:.2f}%')
+    
+    sma_profit = round(total_profit,2)
+    sma_return = round(final_profit_percentage,2)
+    return sma_profit, sma_return
+
 
     pass
 
+
+#function 4: save the results to a json file
 def SaveResults(dictionary):
-    json.dump(dictionary,open('week12/HW5/json_files/results.json','w'))
+    json.dump(dictionary,open('week12/HW5/results.json','w'),indent=4)
 
-
-
+results = {}
+tickers = ['adbe','goog','googl','aapl','fis','fi','intu','pypl','hood','sofi','sqcc']
 for ticker in tickers:
     #isolate the list of prices from the file
-    prices = [float(line) for line in resersed(open('week12/HW5/' + ticker + '_1y_20250404.txt'))]
+    prices = [float(line) for line in reversed(list(open('week12/HW5/' + ticker + '_1y_20250404.txt','r')))]
+    results[ticker+"_prices"]= prices
+
+    # run mean reversion function
+    mr_profit,mr_returns =MeanReversionStrategy(prices, 5)
+    # get the profit and retun % from the functions and save them to a dictionary
+    results[ticker+"_mr_profit"]= mr_profit
+    results[ticker+"_mr_returns"]= mr_returns
+
+    # run simple moving average function
+    sma_profit,sma_returns=SimpleMovingAvg(prices, 5)
+    # get the profit and retun % from the functions and save them to a dictionary
+    results[ticker+"_sma_profit"]= sma_profit
+    results[ticker+"_sma_returns"]= sma_returns
+print(results)
+
+# save the results to a json file
+SaveResults(results)
 
 
-# run mean reversion and simple moving average
-MR_profit,MR_returns =MeanReversionStrategy(prices, ticker)
-SMA_profit,SAM_returns=SimpleMovingAvg(prices, ticker)
 
 
-# get the profit and retun % from the functions and save them to a dictionary
-results[ticker+" prices"]= prices
-results[ticker+" MR profit"]= MR_profit
-results[ticker+" MR returns"]= MR_returns
