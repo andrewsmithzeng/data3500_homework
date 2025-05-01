9cce1f611c6aae15e5019d75c779f9f28fc0629f
Andy Zeng <andrewsmithzeng@gmail.com>
2025-05-01 01:20:46 -0600

    update20250430

---
+++ Final_Project_unfinished/stock_market_trading/tempCodeRunnerFile.py
@@ -0,0 +1,37 @@
+import requests, json
+from datetime import datetime
+print(datetime.now().strftime('%Y-%m-%d')) #%y：25, %Y:2025
+
+stocks = [
+    "AAPL",     # Apple Inc. - Technology
+    "MSFT",     # Microsoft Corporation - Technology
+    "GOOGL",    # Alphabet Inc. - Technology
+    "JPM",      # JPMorgan Chase - Financial
+    "BAC",      # Bank of America - Financial
+    "WMT",      # Walmart - Retail
+    "COST",     # Costco - Retail
+    "JNJ",      # Johnson & Johnson - Healthcare
+    "UNH",      # UnitedHealth - Healthcare
+    "CAT",      # Caterpillar - Industrial
+]
+
+def initial_data_pull(stocks):
+
+    #construct the url for the api call
+    base_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
+    suffix_url = "&outputsize=full&apikey=ACEE72F7S2Z2TN0K"
+    for stock in stocks:
+        url = base_url + stock + suffix_url
+        #make the api call and convert the response to json
+        data = requests.get(url).json()
+
+        #create a list to store the open prices
+        open_prices = []
+        #iterate through the data and extract the open prices
+        for date in data["Time Series (Daily)"].keys():
+            open_price = data["Time Series (Daily)"][date]["1. open"] 
+            open_prices.append(f"{stock}, {date}, {open_price}")
+
+        #store the reserverd open_prices list in a csv file
+        with open("Final_Project_unfinished/stock_market_trading/" + stock + "_open_prices.csv", "w") as f:
+            f.writelines(open_prices[::-1])
