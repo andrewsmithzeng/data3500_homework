ee35cec5696dc654ac81d5d4a116e8663945ab0d
Andy Zeng <andrewsmithzeng@gmail.com>
2025-05-03 06:36:35 -0600

    update20250503

--- Final_Project/cryptocurrency_trading/yfinance/final_project_cryptocurrency_trading.py
+++ Final_Project/cryptocurrency_trading/yfinance/final_project_cryptocurrency_trading.py
@@ -4,51 +4,47 @@ import numpy
 import json
 
 
-#stocks to be analyzed
-stocks = [
-    "AAPL",     # Apple Inc. - Technology
-    "MSFT",     # Microsoft Corporation - Technology
-    "GOOGL",    # Alphabet Inc. - Technology
-    "JPM",      # JPMorgan Chase - Financial
-    "BAC",      # Bank of America - Financial
-    "WMT",      # Walmart - Retail
-    "COST",     # Costco - Retail
-    "JNJ",      # Johnson & Johnson - Healthcare
-    "UNH",      # UnitedHealth - Healthcare
-    "CAT",      # Caterpillar - Industrial
+# Define cryptocurrencies to analyze
+coins = [
+    "BTC-USD",          # Bitcoin - The largest cryptocurrency by market cap
+    "ETH-USD",         # Ethereum - The second largest cryptocurrency
+    "BNB-USD",      # Binance Coin - Binance's native token
+    "ADA-USD",          # Cardano - Smart contract platform
+    "SOL-USD",           # Solana - High-performance blockchain
+    "DOGE-USD",         # Dogecoin - Popular meme cryptocurrency
 ]
 
 #function 1: pull the initial data from the api and store the open prices in a csv file
-def initial_data_pull(stocks):
-    for stock in stocks:
+def initial_data_pull(coins):
+    for coin in coins:
         data = yf.download(
-            stock,
+            coin,
             period = 'max',
             interval = '1d'
-        )['Open',stock]
+        )['Open',coin]
 
-        open_prices = [f'{stock}, {date.strftime("%Y-%m-%d")}, {value}\n'
+        open_prices = [f'{coin}, {date.strftime("%Y-%m-%d")}, {value}\n'
                     for date, value in data.items()]
         
         #store the reserverd open_prices list in a csv file
-        with open("Final_Project/stock_market_trading/yfinance/" + stock + "_open_prices.csv", "w") as f:
+        with open("Final_Project/cryptocurrency_trading/yfinance/" + coin + "_open_prices.csv", "w") as f:
             f.writelines(open_prices)
 
 # #call the initial_data_pull function
-# initial_data_pull(stocks)
+initial_data_pull(coins)
 
 #function 2: append the newest data to the csv file
-def append_data(stocks):
+def append_data(coins):
 
-    for stock in stocks:
+    for coin in coins:
         data = yf.download(
-            stock,
+            coin,
             period = '3mo',
             interval = '1d'
-        )['Open',stock]
+        )['Open',coin]
 
         #get the last day from the csv file
-        with open("Final_Project/stock_market_trading/yfinance/" + stock + "_open_prices.csv", "r") as f:
+        with open("Final_Project/cryptocurrency_trading/yfinance/" + coin + "_open_prices.csv", "r") as f:
             last_day_csv = f.readlines()[-1].split(",")[1].strip()
 
         #create a list to store the open prices need to be appended
@@ -59,14 +55,14 @@ def append_data(stocks):
             if date.strftime("%Y-%m-%d") == last_day_csv:
                 break
             else:
-                open_prices_to_append.append(f'{stock}, {date.strftime("%Y-%m-%d")}, {value}\n')
+                open_prices_to_append.append(f'{coin}, {date.strftime("%Y-%m-%d")}, {value}\n')
 
     #append the open_prices_to_append to the csv file
-        with open("Final_Project/stock_market_trading/yfinance/" + stock + "_open_prices.csv", "a") as f:
+        with open("Final_Project/cryptocurrency_trading/yfinance/" + coin + "_open_prices.csv", "a") as f:
             f.writelines(open_prices_to_append[::-1])
 
 #call the append_data function
-append_data(stocks)
+append_data(coins)
 
 #function 3: mean reversion strategy 
 def MeanReversionStrategy(prices,n):
@@ -77,7 +73,7 @@ def MeanReversionStrategy(prices,n):
     total_profit = 0
 
     for i in range(n,len(prices)-n+1):
-        # set condition to buy when holding no stock.
+        # set condition to buy when holding no coin.
         if buy == None and prices[i] < numpy.mean(prices[i-n:i])*0.98: # without numpy, use sum(prices[i-n:i])/n
             buy = prices[i]
             print(f'buying at {buy}')
@@ -115,7 +111,7 @@ def SimpleMovingAvg(prices, n):
     total_profit = 0
 
     for i in range(n,len(prices)-n+1):
-        # set condition to buy when holding no stock.
+        # set condition to buy when holding no coin.
         if buy == None and prices[i] > numpy.mean(prices[i-n:i]): # without numpy, use sum(prices[i-n:i])/n
             buy = prices[i]
             print(f'buying at {buy}')
@@ -154,7 +150,7 @@ def BollingerBandsStrategy(prices,n):
     total_profit = 0
 
     for i in range(n,len(prices)-n+1):
-        # set condition to buy when holding no stock.
+        # set condition to buy when holding no coin.
         if buy == None and prices[i] < numpy.mean(prices[i-n:i])*0.95: # without numpy, use sum(prices[i-n:i])/n
             buy = prices[i]
             print(f'buying at {buy}')
@@ -192,77 +188,77 @@ def check_last_day_signal(prices,n):
 
     #mean reversion strategy signal
     if last_price < mean*0.98:
-        signals.append(f"based on the mean reversion strategy: you should BUY {stock} stock today")
+        signals.append(f"based on the mean reversion strategy: you should BUY {coin} coin today")
     elif last_price > mean*1.02:
-        signals.append(f"based on the mean reversion strategy: you should SELL {stock} stock today")
+        signals.append(f"based on the mean reversion strategy: you should SELL {coin} coin today")
 
     #simple moving average strategy signal
     if last_price > mean:
-        signals.append(f"based on the simple moving average strategy: you should BUY {stock} stock today")
+        signals.append(f"based on the simple moving average strategy: you should BUY {coin} coin today")
     elif last_price < mean:
-        signals.append(f"based on the simple moving average strategy: you should SELL {stock} stock today")
+        signals.append(f"based on the simple moving average strategy: you should SELL {coin} coin today")
 
     #bollinger bands strategy signal
     if last_price < mean*0.95:
-        signals.append(f"based on the bollinger bands strategy: you should BUY {stock} stock today")
+        signals.append(f"based on the bollinger bands strategy: you should BUY {coin} coin today")
     elif last_price > mean*1.05:
-        signals.append(f"based on the bollinger bands strategy: you should SELL {stock} stock today")
+        signals.append(f"based on the bollinger bands strategy: you should SELL {coin} coin today")
 
     print(signals)
     return signals
 
 #function 7: save the results to a json file
 def SaveResults(dictionary):
-    json.dump(dictionary,open('Final_Project/stock_market_trading/yfinance/results.json','w'),indent=4)
+    json.dump(dictionary,open('Final_Project/cryptocurrency_trading/yfinance/results.json','w'),indent=4)
 
 
 #save the results to a dictionary
 results = {}
 #iterate through the stocks and run the functions
-for stock in stocks:
+for coin in coins:
     #isolate the list of prices from the file
-    prices = [float(line.split(",")[2].strip()) for line in reversed(list(open('Final_Project/stock_market_trading/alphavantage/' + stock + '_open_prices.csv','r')))]
+    prices = [float(line.split(",")[2].strip()) for line in reversed(list(open('Final_Project/cryptocurrency_trading/yfinance/' + coin + '_open_prices.csv','r')))]
     
     # run mean reversion function
     mr_profit,mr_returns =MeanReversionStrategy(prices, 5)
     # get the profit and retun from the functions and save them to a dictionary
-    results[stock+"_mr_profit"]= mr_profit
-    results[stock+"_mr_returns"]= mr_returns
+    results[coin+"_mr_profit"]= mr_profit
+    results[coin+"_mr_returns"]= mr_returns
 
     # run simple moving average function
     sma_profit,sma_returns=SimpleMovingAvg(prices, 5)
     # get the profit and retun from the functions and save them to a dictionary
-    results[stock+"_sma_profit"]= sma_profit
-    results[stock+"_sma_returns"]= sma_returns
+    results[coin+"_sma_profit"]= sma_profit
+    results[coin+"_sma_returns"]= sma_returns
 
     # run bollinger bands function
     bb_profit,bb_returns=BollingerBandsStrategy(prices, 5)
     # get the profit and retun from the functions and save them to a dictionary
-    results[stock+"_bb_profit"]= bb_profit
-    results[stock+"_bb_returns"]= bb_returns
+    results[coin+"_bb_profit"]= bb_profit
+    results[coin+"_bb_returns"]= bb_returns
 
     # run the check_last_day_signal function
     signals = check_last_day_signal(prices, 5)
-    results[stock+"_signals"]= signals
+    results[coin+"_signals"]= signals
 
-    #specify the strategy for each stock that made the highest return
+    #specify the strategy for each coin that made the highest return
     if mr_returns > sma_returns and mr_returns > bb_returns:
-        results[stock+"_best_strategy"]= "mean reversion strategy"
+        results[coin+"_best_strategy"]= "mean reversion strategy"
     elif sma_returns > mr_returns and sma_returns > bb_returns:
-        results[stock+"_best_strategy"]= "simple moving average strategy"
+        results[coin+"_best_strategy"]= "simple moving average strategy"
     else:
-        results[stock+"_best_strategy"]= "bollinger bands strategy"
+        results[coin+"_best_strategy"]= "bollinger bands strategy"
 
-#specify the stock that made the highest return
-most_profitable_stock = ""
+#specify the coin that made the highest return
+most_profitable_coin = ""
 highest_return = float('-inf')
 for k,v in results.items():
     if k.endswith("_returns"):
         if v > highest_return:
             highest_return = v
-            most_profitable_stock = k.split("_")[0]
+            most_profitable_coin = k.split("_")[0]
 
-results["Most_profitable_stock"] = most_profitable_stock
+results["Most_profitable_coin"] = most_profitable_coin
 results["Highest_return"] = highest_return
 
 print(results)
